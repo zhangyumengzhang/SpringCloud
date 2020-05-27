7bb276416baed515c0adbf273d38c78b0e9a30f4
zhangyumengzhang <17301115@bjtu.edu.cn>
2020-05-27 14:09:06 +0800

    Initial commit

---
+++ producer/src/main/java/com/example/producer/ProducerController.java
@@ -0,0 +1,14 @@
+package com.example.producer;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+
+    @RequestMapping("/producer/demo")
+    public String produce(){    
+        return "Hello Word!";
+    }
+}
