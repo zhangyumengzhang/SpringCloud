2fc8da69fb65c9be8909d75ea2c4a8aeae6885c0
zhangyumengzhang <17301115@bjtu.edu.cn>
2020-06-03 10:33:15 +0800

    整合Spring Cloud Bus,实现断路控制,负载均衡

--- config-client/src/main/java/com/example/configclient/ConfigclientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigclientApplication.java
@@ -1,4 +1,4 @@
-package com.example.config;
+package com.example.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
-public class ConfigApplication {
+public class ConfigclientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication.run(ConfigclientApplication.class, args);
     }
 
 }
--- consumer/src/main/java/com/example/consumer/ConsumerController.java
+++ consumer/src/main/java/com/example/consumer/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.example.consumer;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,18 +11,18 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ConsumerController {
 
-    @Bean
-    public RestTemplate restTemplate(){
-        return  new RestTemplate();
-    }
+    @Autowired(required = false)
+    private ConsumerService consumerService;
 
-    @Autowired
-    private RestTemplate restTemplate;
-
+    @HystrixCommand(fallbackMethod = "executeHystrixHandle")
     @RequestMapping("/api/v1/demo/get")
-    public String consumer(){
-//       　交给消费者处理
-        return this.restTemplate.getForObject("http://localhost:5060/producer/demo", String.class);
+    public String consumer() {
+        //交给消费者处理
+        return consumerService.consumer();
     }
 
+    //服务进入保护时，回调方法
+    public String executeHystrixHandle() {
+        return "请稍后再尝试";
+    }
 }
